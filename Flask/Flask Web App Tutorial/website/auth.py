f9ac6db4be54375ae7deed1116a2961a5565dbb9
Sun-Wukhan <navid_khan@icloud.com>
2024-01-28 20:59:18 -0500

    Added SQL Lite and HTML Jinja

--- Flask/Flask Web App Tutorial/website/auth.py
+++ Flask/Flask Web App Tutorial/website/auth.py
@@ -1,13 +1,11 @@
 from flask import Flask 
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login(): 
-    data = request.form
-    print(data)
-    return render_template("login.html", boolean=True)
+    return render_template("login.html", boolean=True, text="Hello World", user="Navid")
 
 @auth.route('/logout')
 def log_out(): 
@@ -15,4 +13,21 @@ def log_out():
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up(): 
+    
+    if request.method == 'POST': 
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+    
+        if len(email) < 4: 
+            flash('Email too short', category='error')
+        elif len(firstName) < 2: 
+            flash('name must be a valid name', category='error')
+        elif password1 != password2: 
+            flash('Password not matching', category='error')
+        else: 
+            flash("YAY!", category='success')
+    
     return render_template("sign_up.html")
+
