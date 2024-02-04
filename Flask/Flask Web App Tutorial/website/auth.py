9324a0182ff1163b6209853a5479b4d55da96228
Sun-Wukhan <navid_khan@icloud.com>
2024-02-04 17:12:04 -0500

    Added Models, Db, Redirection

--- Flask/Flask Web App Tutorial/website/auth.py
+++ Flask/Flask Web App Tutorial/website/auth.py
@@ -1,6 +1,8 @@
-from flask import Flask 
+from flask import Flask, redirect, url_for
 from flask import Blueprint, render_template, request, flash
-
+from .models import User, Note
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
@@ -16,18 +18,21 @@ def sign_up():
     
     if request.method == 'POST': 
         email = request.form.get('email')
-        firstName = request.form.get('firstName')
+        first_name = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
     
         if len(email) < 4: 
             flash('Email too short', category='error')
-        elif len(firstName) < 2: 
+        elif len(first_name) < 2: 
             flash('name must be a valid name', category='error')
         elif password1 != password2: 
             flash('Password not matching', category='error')
         else: 
+            new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1))
+            db.session.add(new_user)
+            db.session.commit()
             flash("YAY!", category='success')
-    
+            return redirect(url_for('views.home'))
     return render_template("sign_up.html")
 
